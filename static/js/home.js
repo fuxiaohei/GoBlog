a4c643add5bf260aa24bf97ab1e4d445b73abb13
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-01-18 20:29:47 +0800

    add go-top button, fix header nav, combine js in front pages

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,62 @@
-$(function () {
+$(document).ready(function () {
+    //fixHeader();
+    topButton();
+    renderMarkdown();
+    initComment();
+});
+
+function fixHeader() {
+    var $nav = $('#nav');
+    var top = $nav.offset().top;
+    console.log(top);
+    $(window).scroll(function () {
+        if (top < $(this).scrollTop()) {
+            $nav.addClass("fixed").removeClass("text-center");
+        } else {
+            $nav.removeClass("fixed").addClass("text-center");
+        }
+    });
+}
+
+function topButton(){
+    var top = $('#nav').offset().top;
+    var $top = $('#go-top');
+    $(window).scroll(function () {
+        if (top < $(this).scrollTop()) {
+            $top.removeClass("hide");
+        } else {
+           $top.addClass('hide');
+        }
+    });
+    $top.on("click",function(){
+        $('body,html').animate({scrollTop:0},500);
+        return false;
+    })
+}
+
+function renderMarkdown() {
+    var $md = $('.markdown');
+    if (window.is_marked) {
+        $md.each(function (i, item) {
+            $(item).html(marked($(item).html().replace(/&gt;/g, '>')));
+        });
+    }
+    var code = $md.find('pre code');
+    if (code.length) {
+        $("<link>").attr({ rel: "stylesheet", type: "text/css", href: "/static/css/highlight.css"}).appendTo("head");
+        $.getScript("/static/lib/highlight.min.js", function () {
+            code.each(function (i, item) {
+                hljs.highlightBlock(item)
+            });
+        });
+    }
+}
+
+function initComment() {
+    var $list = $('#comment-list');
+    if (!$list.length) {
+        return;
+    }
     if (localStorage.getItem("comment-author")) {
         $('#comment-author').val(localStorage.getItem("comment-author"));
         $('#comment-email').val(localStorage.getItem("comment-email"));
@@ -12,7 +70,7 @@ $(function () {
             $('.c-info-fields').removeClass("hide");
         }
     });
-    $('.not-me').on("click",function(){
+    $('.not-me').on("click", function () {
         $('.c-avatar-field').remove();
         $('.c-info-fields').removeClass("hide");
         return false;
@@ -37,35 +95,35 @@ $(function () {
             } else {
                 tpl.find(".c-p-md").remove();
             }
-            tpl.attr("id","comment-"+json.comment.id);
-            if(json.comment.status == "approved"){
+            tpl.attr("id", "comment-" + json.comment.id);
+            if (json.comment.status == "approved") {
                 tpl.find(".c-check").remove();
             }
-            $('#comment-list').append(tpl);
+            $list.append(tpl);
             $('.cancel-reply').trigger("click");
             $('#comment-content').val("");
         } else {
             alert("提交失败!");
         }
     });
-    $('#comment-list').on("click",".c-reply",function(){
+    $list.on("click", ".c-reply", function () {
         $('.reply-md').remove();
         var id = $(this).attr("rel");
-        var pc = $('#comment-'+id);
-        var md = "> @"+pc.find(".c-author").text()+"\n\n";
-        md += "> "+pc.find(".c-content").html()+"\n";
-        $('#comment-content').before('<div class="reply-md markdown">'+marked(md)+'</div>');
+        var pc = $('#comment-' + id);
+        var md = "> @" + pc.find(".c-author").text() + "\n\n";
+        md += "> " + pc.find(".c-content").html() + "\n";
+        $('#comment-content').before('<div class="reply-md markdown">' + marked(md) + '</div>');
         $('#comment-parent').val(id);
-        if($('#comment-show').length){
+        if ($('#comment-show').length) {
             $('#comment-show .enable').trigger("click");
         }
         $('.cancel-reply').show();
         return false;
     });
-    $('.cancel-reply').on("click",function(){
+    $('.cancel-reply').on("click", function () {
         $('.reply-md').remove();
         $('#comment-parent').val(0);
         $(this).hide();
         return false;
     });
-});
+}
