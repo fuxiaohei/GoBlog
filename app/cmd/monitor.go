4e7c4db1c7cf1b61da97f60ea7ec5f9e1b259cf6
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-02-10 22:50:02 +0800

    gofmt

--- app/cmd/monitor.go
+++ app/cmd/monitor.go
@@ -1,21 +1,21 @@
 package cmd
 
 import (
-	"runtime"
-	"github.com/fuxiaohei/GoBlog/app/utils"
-	"time"
 	"fmt"
+	"github.com/fuxiaohei/GoBlog/app/utils"
+	"runtime"
+	"time"
 )
 
 type monitorStats struct {
 	NumGoroutine int
 	MemAllocated string
-	MemMalloc string
-	MemTotal string
-	MemSys string
-	MemHeap string
-	MemGc string
-	LastGcTime string
+	MemMalloc    string
+	MemTotal     string
+	MemSys       string
+	MemHeap      string
+	MemGc        string
+	LastGcTime   string
 }
 
 func ReadMemStats() *monitorStats {
@@ -28,7 +28,7 @@ func ReadMemStats() *monitorStats {
 	ms.MemSys = utils.FileSize(int64(m.Sys))
 	ms.MemHeap = utils.FileSize(int64(m.HeapAlloc))
 	ms.MemMalloc = utils.FileSize(int64(m.Mallocs))
-	ms.LastGcTime = fmt.Sprintf("%.1fs",float64(time.Now().UnixNano() - int64(m.LastGC))/1000/1000/1000)
+	ms.LastGcTime = fmt.Sprintf("%.1fs", float64(time.Now().UnixNano()-int64(m.LastGC))/1000/1000/1000)
 	ms.MemGc = utils.FileSize(int64(m.NextGC))
 	return ms
 }
