00b2e0ee67c61a8e4ed3ceed9a5fdd280acc1610
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-02-15 23:23:15 +0800

    add code comments

--- app/model/content.go
+++ app/model/content.go
@@ -17,6 +17,7 @@ var (
 	contentsIndex map[string][]int
 )
 
+// Content instance, defines content data items.
 type Content struct {
 	Id    int
 	Title string
@@ -29,28 +30,35 @@ type Content struct {
 	EditTime   int64
 	UpdateTime int64
 
+	// IsComment opens or closes comment
 	IsComment bool
-	IsLinked  bool
+	// IsLinked makes pager link as top level link /link.html
+	IsLinked bool
 	//IsTop     bool
 
 	AuthorId int
 
+	// Template makes pager use own template file
 	Template string
 
 	Type   string
 	Status string
+
+	// Format defines the content text format type. Now only support markdown.
 	Format string
 
 	Comments []*Comment
 	Hits     int
 }
 
-// get content tags in a string.
+// TagString returns content tags in a string that's joined by ",".
 func (cnt *Content) TagString() string {
 	return strings.Join(cnt.Tags, ",")
 }
 
-// get content link.
+// Link returns content link as {type}/{id}/{slug}.html.
+// If content isn't published, return "#".
+// If content is page and top linked, return {slug}.html as top level link.
 func (cnt *Content) Link() string {
 	if cnt.Status != "publish" {
 		return "#"
@@ -61,7 +69,8 @@ func (cnt *Content) Link() string {
 	return fmt.Sprintf("/%s/%d/%s.html", cnt.Type, cnt.Id, cnt.Slug)
 }
 
-// get content text.
+// Content returns whole content text.
+// If enable go-markdown, return markdown-rendered content.
 func (cnt *Content) Content() string {
 	txt := strings.Replace(cnt.Text, "<!--more-->", "", -1)
 	if GetSetting("enable_go_markdown") == "true" {
@@ -70,7 +79,9 @@ func (cnt *Content) Content() string {
 	return txt
 }
 
-// get content summary.
+// Summary returns content summary.
+// Summary text means the part before page-break <!--more-->.
+// It can be go-markdown rendered.
 func (cnt *Content) Summary() string {
 	text := strings.Split(cnt.Text, "<!--more-->")[0]
 	if GetSetting("enable_go_markdown") == "true" {
@@ -79,7 +90,8 @@ func (cnt *Content) Summary() string {
 	return text
 }
 
-// get content comments number.
+// CommentNum returns content comments number.
+// If comment are checking or, its parent are lost, do not count it.
 func (cnt *Content) CommentNum() int {
 	max := len(cnt.Comments)
 	for _, c := range cnt.Comments {
@@ -90,7 +102,8 @@ func (cnt *Content) CommentNum() int {
 	return max
 }
 
-// change slug and check unique.
+// ChangeSlug changes content's slug.
+// It checks whether this slug is unique.
 func (cnt *Content) ChangeSlug(slug string) bool {
 	c2 := GetContentBySlug(slug)
 	if c2 == nil {
@@ -104,17 +117,17 @@ func (cnt *Content) ChangeSlug(slug string) bool {
 	return true
 }
 
-// get content author user.
+// User returns content author user instance.
 func (cnt *Content) User() *User {
 	return GetUserById(cnt.AuthorId)
 }
 
-// get a content by given id.
+// GetContentById gets a content by given id.
 func GetContentById(id int) *Content {
 	return contents[id]
 }
 
-// get a content by given slug.
+// GetContentBySlug gets a content by given slug.
 func GetContentBySlug(slug string) *Content {
 	for _, c := range contents {
 		if c.Slug == slug {
@@ -124,6 +137,7 @@ func GetContentBySlug(slug string) *Content {
 	return nil
 }
 
+// GetPublishArticleList gets published article list and pager.
 func GetPublishArticleList(page, size int) ([]*Content, *utils.Pager) {
 	index := contentsIndex["article-publish"]
 	pager := utils.NewPager(page, size, len(index))
@@ -141,7 +155,7 @@ func GetPublishArticleList(page, size int) ([]*Content, *utils.Pager) {
 	return articles, pager
 }
 
-// get articles list.
+// GetArticleList gets articles list and pager no matter article status.
 func GetArticleList(page, size int) ([]*Content, *utils.Pager) {
 	index := contentsIndex["article"]
 	pager := utils.NewPager(page, size, len(index))
@@ -155,7 +169,8 @@ func GetArticleList(page, size int) ([]*Content, *utils.Pager) {
 	return articles, pager
 }
 
-// get pages list.
+// GetPageList gets pages list and pager no matter page status.
+// In common cases, no need to get a list or pagers for public page.
 func GetPageList(page, size int) ([]*Content, *utils.Pager) {
 	index := contentsIndex["page"]
 	pager := utils.NewPager(page, size, len(index))
@@ -169,7 +184,8 @@ func GetPageList(page, size int) ([]*Content, *utils.Pager) {
 	return pages, pager
 }
 
-// create new content.
+// CreateContent creates new content.
+// t means content type, article or page.
 func CreateContent(c *Content, t string) (*Content, error) {
 	c2 := GetContentBySlug(c.Slug)
 	if c2 != nil {
@@ -190,16 +206,17 @@ func CreateContent(c *Content, t string) (*Content, error) {
 	return c, nil
 }
 
-// save changed content.
+// SaveContent saves changed content.
+// It will re-generate related indexes.
 func SaveContent(c *Content) {
 	c.EditTime = utils.Now()
 	generatePublishArticleIndex()
 	go SyncContent(c)
 }
 
-// remove a content.
-// not delete file, just change status to DELETE.
-// it can't be loaded in memory from json.
+// RemoveContent removes a content.
+// Not delete file really, just change status to DELETE.
+// This content can't be loaded in memory from storage json.
 func RemoveContent(c *Content) {
 	delete(contents, c.Id)
 	for i, id := range contentsIndex[c.Type] {
@@ -213,29 +230,30 @@ func RemoveContent(c *Content) {
 	go SyncContent(c)
 }
 
-// write a content to json.
+// SyncContent writes a content to storage json.
 func SyncContent(c *Content) {
 	key := fmt.Sprintf("content/%s-%d", c.Type, c.Id)
 	Storage.Set(key, c)
 }
 
-// write all contents to json.
+// SyncContents writes all contents to storage json.
 func SyncContents() {
 	for _, c := range contents {
 		SyncContent(c)
 	}
 }
 
-// load all contents.
-// generate indexes.
+// LoadContents loads all contents, then generates indexes.
 func LoadContents() {
 	contents = make(map[int]*Content)
 	contentsIndex = make(map[string][]int)
 	contentMaxId = 0
 	articleIndex := make([]int, 0)
 	pageIndex := make([]int, 0)
+	// walk files in directory
 	filepath.Walk(filepath.Join(Storage.dir, "content"), func(_ string, info os.FileInfo, err error) error {
 		if err == nil {
+			// ignore dir and sub-dir
 			if info.IsDir() {
 				return nil
 			}
@@ -243,6 +261,7 @@ func LoadContents() {
 			file := strings.Replace("content/"+info.Name(), filepath.Ext(info.Name()), "", -1)
 			Storage.Get(file, c)
 			if c.Id > 0 {
+				// ignore DELETE status
 				if c.Status != "DELETE" {
 					contents[c.Id] = c
 					if c.Type == "article" {
@@ -259,6 +278,7 @@ func LoadContents() {
 		}
 		return nil
 	})
+	// sort indexes as desc order
 	sort.Sort(sort.Reverse(sort.IntSlice(articleIndex)))
 	sort.Sort(sort.Reverse(sort.IntSlice(pageIndex)))
 	contentsIndex["article"] = articleIndex
@@ -277,6 +297,8 @@ func generatePublishArticleIndex() {
 	contentsIndex["article-publish"] = arr
 }
 
+// StartContentsTimer starts some timers for contents.
+// An hour timer is for sync all contents because their hits number are changing.
 func StartContentsTimer() {
 	time.AfterFunc(time.Duration(1)*time.Hour, func() {
 		println("write contents in 1 hour timer")
